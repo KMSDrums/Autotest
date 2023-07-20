7f70c660cb1c7f2b821d7af69ada8ec27ea8b1cc
EugeneKMS <evgenikomissarov@yandex.ru>
2023-07-20 18:20:05 +0300

    v.1.0.1

--- Autotest/authorization.py
+++ Autotest/authorization.py
@@ -1,11 +1,13 @@
 from playwright.sync_api import sync_playwright
 import data
+import pyperclip
+import json
 
 email = data.email()
 password = data.password()
 with sync_playwright() as p:
     # launch browser
-    browser = p.chromium.launch()
+    browser = p.chromium.launch(headless=False)
     page = browser.new_page()
     # go to site
     page.goto("https://account.getblock.io/sign-in-email")
@@ -19,16 +21,38 @@ with sync_playwright() as p:
     # login
     page.get_by_test_id("signInButton").click()
     page.wait_for_timeout(5000)
-    # choice protocol and network
-    page.get_by_test_id("protocolDropdown").click()
-    page.query_selector("text=Bitcoin").click()
-    page.get_by_test_id("networkDropdown").click()
-    page.query_selector("text=Mainnet").click()
-    page.get_by_test_id("createEndpointButton").click()
-    page.wait_for_timeout(5000)
-    # check response
-    check_visibility = page.get_by_test_id("endpoint").is_visible()
-    if check_visibility:
-        print("Success")
-    page.screenshot(path="screenshot.png", full_page=True)
+    # copy apikey
+    page.get_by_test_id("apikeyButton").click()
+    page.wait_for_timeout(1000)
+    page.get_by_role("button", name="Copy", exact=True).click()
+    apikey = pyperclip.paste()
+    print(apikey)
+    # send request
+    context = browser.new_context(base_url="https://btc.getblock.io")
+    api_request_context = context.request
+    page = context.new_page()
+    to_send = """
+          {
+            "jsonrpc": "2.0",
+            "id": "healthcheck",
+            "method": "getmininginfo",
+            "params": []
+            }
+        """
+    response = api_request_context.post(
+        "/mainnet/",
+        headers={
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+            "x-api-key": apikey
+        },
+        data=to_send,
+    )
+    try:
+        print(response)
+        data = json.loads(response)
+    except:
+        # Still might fail sometimes
+        data = None
+    print(data)
     browser.close()
